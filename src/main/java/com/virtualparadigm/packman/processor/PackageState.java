c92f4921ecf52ed319819e86211ea0f2370cc57b
unknown <kapil.dhingra@KapilLaptop.jerichosystems.local>
2015-09-17 00:41:29 -0500

    removing wrong project committed

--- src/main/java/com/virtualparadigm/packman/processor/PackageState.java
+++
@@ -1,6 +0,0 @@
-package com.virtualparadigm.packman.processor;
-
-public enum PackageState
-{
-	STARTED, INITIALIZED, VALIDATED, CONFIGURED, DEPLOYED, AUTORUN_COMPLETE, FINALIZED, INSTALLED;
-}
--- src/main/scripts/jpm.sh
+++
@@ -1,1 +0,0 @@
-java -jar jpackage-manager-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
